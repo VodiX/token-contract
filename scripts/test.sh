31fe07874d794449cace478909eb57bf75c4c558
Nidish Ramakrishnan <nramakrishnan@techracers.io>
2019-01-16 20:07:01 +0530

    Chore: Dependencies fix

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,31 +1,72 @@
 #!/usr/bin/env bash
 
-# Based on https://github.com/OpenZeppelin/zeppelin-solidity/blob/master/scripts/test.sh
+# Exit script as soon as a command fails.
+set -o errexit
 
 # Executes cleanup function at script exit.
 trap cleanup EXIT
 
 cleanup() {
-  # Kill the ganache-cli instance that we started (if we started one and if it's still running).
-  if [ -n "$ganache_cli_pid" ] && ps -p $ganache_cli_pid > /dev/null; then
-    kill -9 $ganache_cli_pid
+  # Kill the ganache instance that we started (if we started one and if it's still running).
+  if [ -n "$ganache_pid" ] && ps -p $ganache_pid > /dev/null; then
+    kill -9 $ganache_pid
   fi
 }
 
-ganache_cli_running() {
-  nc -z localhost 8545
-}
-
-run_ganache_cli() {
-  ganache-cli --account="0x129c5743e4cc428a6feb3c3963fe06ad73d485acd8bb4b9cb090f56b961a934e, 100000000000000000000000000000000" --account="0xa76d83a00b24c23b881e2891312f2c1bef302922752fc2953cf39c3dfcd0f738, 100000000000000000000000000000000" --account="0x05a23befc0635f33e7c50f64084a711b92b15321d04b070f82194d7c872a7161, 100000000000000000000000000000000" --account="0xd582dd6615191be88ec76c86bdc0e26cf5d7544cf889415e6d2805e5a8408675, 100000000000000000000000000000000" --account="0x258c16c5bf24b74a56943fb1a57ba7cb04f6c24324d9256f7514c0838437e94a, 100000000000000000000000000000000" --account="0x7e2b9438c59e8f7e11e6c9060ae595a5645ce040ba1a8659926c39a32284c066, 100000000000000000000000000000000" --account="0xd10e4f31a007d8724656cd0af38530f0d0f2bc0895cc01599eb0ff985a7a378b, 100000000000000000000000000000000" --account="0x890b7db32d5a01f65e6858c65251eab55ed64d9f0f3b76568783df3b54b75bb3, 100000000000000000000000000000000" --account="0x8714de973f2e9585bec1f11a01a2ae1cf88cfb73ec12ed1f62c60de8aa9da32d, 100000000000000000000000000000000" --account="0x1de31fb89294be0f75bbb92b2bfa4436f9bc1c6082d28d51cdce69ca1ca80a49, 100000000000000000000000000000000" > /dev/null &
-}
-
-if ganache_cli_running; then
-  echo "Using existing ganache-cli instance"
+if [ "$SOLIDITY_COVERAGE" = true ]; then
+  ganache_port=8555
 else
-  echo "Starting our own ganache-cli instance"
-  run_ganache_cli
-  ganache_cli_pid=$!
+  ganache_port=8545
 fi
 
-node_modules/.bin/truffle test --network development"$@"
+ganache_running() {
+  nc -z localhost "$ganache_port"
+}
+
+start_ganache() {
+  # We define 10 accounts with balance 1M ether, needed for high-value tests.
+  local accounts=(
+    --account="0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501200,1000000000000000000000000"
+    --account="0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501201,1000000000000000000000000"
+    --account="0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501202,1000000000000000000000000"
+    --account="0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501203,1000000000000000000000000"
+    --account="0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501204,1000000000000000000000000"
+    --account="0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501205,1000000000000000000000000"
+    --account="0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501206,1000000000000000000000000"
+    --account="0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501207,1000000000000000000000000"
+    --account="0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501208,1000000000000000000000000"
+    --account="0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501209,1000000000000000000000000"
+  )
+
+  if [ "$SOLIDITY_COVERAGE" = true ]; then
+    node_modules/.bin/testrpc-sc --gasLimit 0xfffffffffff --port "$ganache_port" "${accounts[@]}" > /dev/null &
+  else
+    node_modules/.bin/ganache-cli --gasLimit 0xfffffffffff "${accounts[@]}" > /dev/null &
+  fi
+
+  ganache_pid=$!
+}
+
+if ganache_running; then
+  echo "Using existing ganache instance"
+else
+  echo "Starting our own ganache instance"
+  start_ganache
+fi
+
+if [ "$SOLC_NIGHTLY" = true ]; then
+  echo "Downloading solc nightly"
+  wget -q https://raw.githubusercontent.com/ethereum/solc-bin/gh-pages/bin/soljson-nightly.js -O /tmp/soljson.js && find . -name soljson.js -exec cp /tmp/soljson.js {} \;
+fi
+
+truffle version
+
+if [ "$SOLIDITY_COVERAGE" = true ]; then
+  node_modules/.bin/solidity-coverage
+
+  if [ "$CONTINUOUS_INTEGRATION" = true ]; then
+    cat coverage/lcov.info | node_modules/.bin/coveralls
+  fi
+else
+  node_modules/.bin/truffle test "$@"
+fi
--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,22 @@
 module.exports = {
-	// See <http://truffleframework.com/docs/advanced/configuration>
-	// to customize your Truffle configuration!
+  networks: {
+    development: {
+      host: 'localhost',
+      port: 8545,
+      network_id: '*', // eslint-disable-line camelcase
+    },
+    coverage: {
+      host: 'localhost',
+      network_id: '*', // eslint-disable-line camelcase
+      port: 8555,
+      gas: 0xfffffffffff,
+      gasPrice: 0x01,
+    },
+  },
+
+  compilers: {
+    solc: {
+      version: '0.5.0',
+    },
+  },
 };
