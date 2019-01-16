6fe213eb5573d580b4a37ba1640088aa97f9119f
Nidish Ramakrishnan <nramakrishnan@techracers.io>
2019-01-16 20:05:18 +0530

    Add: Crowdsale with ether allocation feature

---
+++ contracts/crowdsale/AllocateEther.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.5.0;
+
+import "../../zeppelin-solidity/contracts/crowdsale/distribution/FinalizableCrowdsale.sol";
+
+/**
+ * @title AllocateEther
+ * @dev Extension of Crowdsale where funds are kept in the crowdsale
+ * contract until finalize is called.
+ */
+contract AllocateEther is FinalizableCrowdsale {
+    // send ether to the fund collection wallet
+  // called when owner calls finalize()
+  function _finalization() internal {
+    super._forwardFunds();
+    super._finalization();
+  }
+
+  // overriding to keep funds in contract
+  function _forwardFunds() internal {
+
+  }
+}
+
