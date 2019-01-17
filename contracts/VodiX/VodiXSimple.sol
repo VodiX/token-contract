7a715b776164f5c7142d01074d1119392e6c6adb
Nidish Ramakrishnan <nramakrishnan@techracers.io>
2019-01-17 16:50:02 +0530

    Add: VodiX simple token contract

---
+++ contracts/VodiX/VodiXSimple.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.5.0;
+
+import "../../zeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "../../zeppelin-solidity/contracts/token/ERC20/ERC20Burnable.sol";
+import "../../zeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
+
+contract VodiXSimple is ERC20, ERC20Burnable, ERC20Detailed {
+
+    uint8 public constant DECIMALS = 18;
+    uint256 public constant INITIAL_SUPPLY = 1000000000 * (10 ** uint256(DECIMALS));
+
+    /**
+     * @dev Constructor that gives msg.sender all of existing tokens.
+     */
+    constructor () public ERC20Detailed("Vodi X", "VDX", DECIMALS) {
+        _mint(msg.sender, INITIAL_SUPPLY);
+    }
+}
