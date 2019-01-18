4d1f2608f30114a2f5ea1170f17c0deddf0f1604
Nidish Ramakrishnan <nramakrishnan@techracers.io>
2019-01-18 12:52:56 +0530

    Cleanup: VodiXSimple contract

--- contracts/VodiX/VodiXSimple.sol
+++ contracts/VodiX/VodiXSimple.sol
@@ -1,18 +1,17 @@
 pragma solidity ^0.5.0;
 
-import "../token/ERC20/ERC20.sol";
-import "../token/ERC20/ERC20Burnable.sol";
-import "../token/ERC20/ERC20Detailed.sol";
+import "../../zeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "../../zeppelin-solidity/contracts/token/ERC20/ERC20Burnable.sol";
+import "../../zeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
 
 contract VodiXSimple is ERC20, ERC20Burnable, ERC20Detailed {
 
-    uint8 public constant DECIMALS = 18;
-    uint256 public constant INITIAL_SUPPLY = 1000000000 * (10 ** uint256(DECIMALS));
+    uint256 public constant INITIAL_SUPPLY = 1000000000 * (10 ** uint256(18));
 
     /**
      * @dev Constructor that gives msg.sender all of existing tokens.
      */
-    constructor () public ERC20Detailed("Vodi X", "VDX", DECIMALS) {
+    constructor () ERC20Detailed("Vodi X", "VDX", 18) public {
         _mint(msg.sender, INITIAL_SUPPLY);
     }
 }
--- test/VodixSimple.test.js
+++ test/VodixSimple.test.js
@@ -15,20 +15,16 @@ contract('VodiXSimple', function ([owner, recipient, anotherAccount, ...otherAcc
     const _symbol = 'VDX';
     const _decimals = new BN(18);
   
-    beforeEach(async function () {
-      this.detailedERC20 = await VodiXSimple.new();
-    });
-  
     it('has a name', async function () {
-      (await this.detailedERC20.name()).should.be.equal(_name);
+      (await this.token.name()).should.be.equal(_name);
     });
   
     it('has a symbol', async function () {
-      (await this.detailedERC20.symbol()).should.be.equal(_symbol);
+      (await this.token.symbol()).should.be.equal(_symbol);
     });
   
     it('has an amount of decimals', async function () {
-      (await this.detailedERC20.decimals()).should.be.bignumber.equal(_decimals);
+      (await this.token.decimals()).should.be.bignumber.equal(_decimals);
     });
   });
 
